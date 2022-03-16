71079a2e8d574d8e05b4de286738892e3fba09d1
mckay-moulton <79293626+mckay-moulton@users.noreply.github.com>
2022-03-15 21:17:13 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using LaytonTemple.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaytonTemple.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Sign_Up()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Sign_Up(AvailableTimes response)
+        {
+            return View("Form", response);
+        }
+
+    }
+}
---
+++ Models/AvailableTimes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaytonTemple.Models
+{
+    public class AvailableTimes
+    {
+        public DateTime TimeSlot { get; set; }
+    }
+}
